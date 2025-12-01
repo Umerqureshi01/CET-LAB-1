2141eb968f9bac6c769cff6d3e4f76c53433203f
UMER QURESHI <linkedina522@gmail.com>
2025-12-01 11:39:13 -0800

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+	cout<<"welcome to the first lab!"<<endl;
+	cout<<"you will learn basic output today"<<endl;
+	cout<<"let's start with basic messages"<<endl;	
+	return(0);
+}
